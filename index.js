8b9b7d6ad15552148ca835a240197d7a3036881c
wangm <wangm@samples.cn>
2017-03-16 15:42:09 +0800

    1.0.1 fixed a pool bug

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 const assert = require('assert');
 
-if (assert.isTrue) {
+if (!assert.isTrue) {
 	assert.isTrue = (expected, message) => {
 		assert.equal(true, expected. message);
 	};
 }
-if (assert.isStrictTrue) {
+if (!assert.isStrictTrue) {
 	assert.isStrictTrue = (expected, message) => {
 		assert.strictEqual(true, expected. message);
 	};
 }
-if (assert.isFalse) {
+if (!assert.isFalse) {
 	assert.isFalse = (expected, message) => {
 		assert.equal(false, expected. message);
 	};
 }
-if (assert.isStrictFalse) {
+if (!assert.isStrictFalse) {
 	assert.isStrictFalse = (expected, message) => {
 		assert.strictEqual(false, expected. message);
 	};
 }
-if (assert.isNaN) {
+if (!assert.isNaN) {
 	assert.isNaN = (expected, message) => {
 		assert.isStrictTrue(isNaN(expected). message);
 	};
 }
-if (assert.isStrictNaN) {
+if (!assert.isStrictNaN) {
 	assert.isStrictNaN = (expected, message) => {
 		assert.isStrictTrue(isNaN(expected) && typeof expected === 'number'. message);
 	};
 }
-if (assert.isNotNaN) {
+if (!assert.isNotNaN) {
 	assert.isNotNaN = (expected, message) => {
 		assert.isStrictFalse(isNaN(expected). message);
 	};
 }
-if (assert.isNotStrictNaN) {
+if (!assert.isNotStrictNaN) {
 	assert.isNotStrictNaN = (expected, message) => {
 		assert.isStrictFalse(isNaN(expected) && typeof expected === 'number'. message);
 	};
